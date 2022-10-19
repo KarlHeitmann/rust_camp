2dcbacd9999c04fe0c246c0025bfaa68156e2c2a
Karl Heitmann <khf1988@gmail.com>
2022-10-18 21:43:39 -0300

    finish lesson2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Welcome to your number adding machine!");
+
+    println!("Please enter your first number:");
+    let mut first_number = String::new();
+    io::stdin().read_line(&mut first_number).expect("Failed to read line"); // Some basic error handling.
+
+    println!("Please enter your second number:");
+    let mut second_number = String::new();
+    io::stdin().read_line(&mut second_number).expect("Failed to read line");
+
+    // let mut a_number = "1";
+
+    /*
+    let first_number: i32 = |x: String| x.trim().parse::<i32>().unwrap().expect("Please type a number!");
+    let first_number: i32 = |x: String| x.trim().parse::<i32>().unwrap().expect("Please type a number!");
+    */
+    let first_number: i32 = first_number.trim().parse().expect("Please type a number!");
+    let second_number: i32 = second_number.trim().parse().expect("Please type a number!");
+
+    println!("we are here");
+
+    println!("The sum of your numbers is: {}", first_number + second_number);
 }
